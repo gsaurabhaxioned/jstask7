10e97968aabdbd9b6f6a339f7f529aadff762c99
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-14 12:45:38 +0530

    js code added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,10 @@
-/* Author: 
+const device = document.querySelectorAll('.device');
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for (let i = 0; i < device.length; i++) {
+    device[i].addEventListener('mouseover', function () {
+        for (let j = 0; j < device.length; j++) {
+            device[j].style.flexBasis = "25.3%";
+        }
+        device[i].style.flexBasis = "74.3%";
+    });
+}
