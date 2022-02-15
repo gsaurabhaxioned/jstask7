3fcbc12c4315c1fd8630caa3d8900eb3d5c4bc16
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-15 11:43:51 +0530

    mouse out effect added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,14 @@ const device = document.querySelectorAll('.device');
 for (let i = 0; i < device.length; i++) {
     device[i].addEventListener('mouseover', function () {
         for (let j = 0; j < device.length; j++) {
-            device[j].style.flexBasis = "25.3%";
+            device[j].style.flexBasis = "26%";
         }
-        device[i].style.flexBasis = "74.3%";
+        device[i].style.flexBasis = "74%";
+    });
+
+    device[i].addEventListener('mouseout', function () {
+        for (let j = 0; j < device.length; j++) {
+            device[j].style.flexBasis = "50%";
+        }
     });
 }
